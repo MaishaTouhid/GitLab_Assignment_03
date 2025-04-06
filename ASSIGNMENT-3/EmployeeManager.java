2e3e66df55b34001cf1726e825d411fe094d74ef
Maisha <maishatouhid1982004@gmail.com>
2025-04-06 21:47:53 +0600

    Update Code Style for Better Consistenc

--- ASSIGNMENT-3/EmployeeManager.java
+++ ASSIGNMENT-3/EmployeeManager.java
@@ -9,44 +9,47 @@ public class EmployeeManager {
             try {
                 BufferedReader r = new BufferedReader(
                         new InputStreamReader(
-                                new FileInputStream("employees.txt")));
+                            new FileInputStream("employees.txt")));
                 String l = r.readLine();
                 String e[] = l.split(",");
                 for (String emp : e) {
                     System.out.println(emp);
                 }
-            } catch (Exception e) {}
+            } catch (Exception e) {
             System.out.println("Data Loaded.");
+            }
         } else if (args[0].equals("s")) {
             System.out.println("Loading data ...");
             try {
                 BufferedReader r = new BufferedReader(
                         new InputStreamReader(
-                                new FileInputStream("employees.txt")));
+                            new FileInputStream("employees.txt")));
                 String l = r.readLine();
                 System.out.println(l);
                 String e[] = l.split(",");
                 Random rand = new Random();
                 int idx = rand.nextInt(e.length);
                 System.out.println(e[idx]);
-            } catch (Exception e) {}
+            } catch (Exception e) {
             System.out.println("Data Loaded.");
+            }
         } else if (args[0].contains("+")) {
             System.out.println("Loading data ...");
             try {
                 BufferedWriter w = new BufferedWriter(
-                        new FileWriter("employees.txt", true));
+                    new FileWriter("employees.txt", true));
                 String n = args[0].substring(1);
                 w.write(", " + n);
                 w.close();
-            } catch (Exception e) {}
+            } catch (Exception e) {
             System.out.println("Data Loaded.");
+            }
         } else if (args[0].contains("?")) {
             System.out.println("Loading data ...");
             try {
                 BufferedReader r = new BufferedReader(
                         new InputStreamReader(
-                                new FileInputStream("employees.txt")));
+                            new FileInputStream("employees.txt")));
                 String l = r.readLine();
                 String e[] = l.split(",");
                 boolean found = false;
@@ -57,8 +60,9 @@ public class EmployeeManager {
                         found = true;
                     }
                 }
-            } catch (Exception e) {}
+            } catch (Exception e) {
             System.out.println("Data Loaded.");
+            }
         } else if (args[0].contains("c")) {
             System.out.println("Loading data ...");
             try {
@@ -80,8 +84,9 @@ public class EmployeeManager {
                     }
                 }
                 System.out.println(count + " word(s) found " + chars.length);
-            } catch (Exception e) {}
+            } catch (Exception e) {
             System.out.println("Data Loaded.");
+            }
         } else if (args[0].contains("u")) {
             System.out.println("Loading data ...");
             try {
@@ -100,8 +105,9 @@ public class EmployeeManager {
                         new FileWriter("employees.txt"));
                 w.write(String.join(",", e));
                 w.close();
-            } catch (Exception e) {}
+            } catch (Exception e) {
             System.out.println("Data Updated.");
+            }
         } else if (args[0].contains("d")) {
             System.out.println("Loading data ...");
             try {
@@ -117,8 +123,9 @@ public class EmployeeManager {
                         new FileWriter("employees.txt"));
                 w.write(String.join(",", list));
                 w.close();
-            } catch (Exception e) {}
+            } catch (Exception e) {
             System.out.println("Data Deleted.");
+            }
         }
     }
 }
